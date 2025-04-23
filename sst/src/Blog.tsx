3a79d1fca7bf07699cd7a707ffed4b4d83890ba6
Yisel Lorena <yiselorenasalzar@gmail.com>
2025-04-23 15:36:26 -0500

    ajustes de estilo

--- sst/src/Blog.tsx
+++ sst/src/Blog.tsx
@@ -1,26 +1,49 @@
 import React from "react";
-import "./Blog.css"
-
+import "./Blog.css";
 
 const Blog: React.FC = () => {
+  const posts = [
+    {
+      id: 1,
+      title: "TITULO 1",
+      description: "DESCRIPCIÓN 1",
+      image: "IMAGEN DE LA PUBLICACION 1"
+    },
+    {
+      id: 2,
+      title: "TITULO 2",
+      description: "DESCRIPCIÓN 2",
+      image: "IMAGEN DE LA PUBLICACION 2"
+    },
+    {
+      id: 3,
+      title: "TITULO 3",
+      description: "DESCRIPCIÓN 3",
+      image: "IMAGEN DE LA PUBLICACION 3"
+    }
+  ];
+
   return (
     <section className="blog">
-      <input className="titulo" placeholder="TITULO" disabled/>
-        <div className="image">
-          <span>IMAGEN DE LA PUBLICACION</span>
+      <h1 className="h1">Bienvenidos a mi Blog</h1> {/* Título centrado */}
+      {posts.map((post) => (
+        <div className="contenido" key={post.id}>
+          <input className="titulo" placeholder={post.title} disabled />
+          <div className="image">
+            <span>{post.image}</span>
+          </div>
+
+          <input className="descripcion" placeholder={post.description} disabled />
+
+          <div className="post-icons">
+            <span>❤️</span>
+            <span>💬</span>
+            <span>🔖</span>
+          </div>
+
+          <button className="comentarios">SECCION DE COMENTARIOS</button>
         </div>
-
-      <input className="descripcion" placeholder="DESCRIPCIÓN" disabled/>
-
-        <div className="post-icons">
-          <span>❤️</span>
-          <span>💬</span>
-          <span>🔖</span>
-        </div>
-
-
-
-       <button className="comentarios">SECCION DE COMENTARIOS</button>
+      ))}
     </section>
   );
 };
